5349427e2f23c8a59dc26cd9111ccc004938c563
siva gopal <sivagopal.b@gmail.com>
2019-07-16 16:05:39 +0100

    Jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -6,5 +6,8 @@ pipeline {
         sh '/usr/bin/pip install --user -r requirements.txt'
       }
     }
+    stage('deploy') {
+       sh 'chef-solo -c ${HOME}/chef-repo/solo.rb -j ${HOME}/chef-repo/portal-setup.json --log-level debug > logFile'
+    }
   }
 }
--- solo.rb
+++ solo.rb
@@ -1,6 +1,6 @@
-file_cache_path    "/home/csocprt01/python-self/cache"
-file_backup_path   "/home/csocprt01/python-self"
-cookbook_path ["/home/csocprt01/python-self/csoc-cookbooks"]
+file_cache_path    "${HOME}/chef-repo/cache"
+file_backup_path   "${HOME}/chef-repo/python-self"
+cookbook_path ["${HOME}/chef-repo/csoc-cookbooks"]
 # An alternative and more useable method is to use ruby code to get directory path
 #       cookbook_path File.expand_path('/cookbooks')
 log_level :info
